70f711eb2d87710f42d8c626f3da1dd16be34d52
Hongyu Zhu <serailhydra@gmail.com>
2020-10-04 17:20:23 +0800

    Update run.sh

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,1 +1,1 @@
-python3 predict.py --ptx_file ../examples/samples/MatMul/_/my_kernel.cc.ptx
+python3 predict.py --cu-file ../examples/samples/MatMul/_/my_kernel.cc --ptx_file ../examples/samples/MatMul/_/my_kernel.cc.ptx
