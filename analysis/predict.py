959c19afaa75c0da23ecbb62bc36facaf90bf851
Hongyu Zhu <serailhydra@gmail.com>
2020-10-06 10:17:29 +0800

    Update predict.py

--- analysis/predict.py
+++ analysis/predict.py
@@ -4,18 +4,44 @@
 import os, argparse
 import dag
 from dag import *
+from gpu_config import *
 
-def estimate_TB(ptx_parser, hw_config)
+def get_TB_size_count(cu_file):
+    # return the # of threads in one TB and # of TBs
+    # this information is written in the .cu source file, with lines that contain "[thread_extent]"
+    
+    TB_count_x = 1
+    TB_count_y = 1
+    TB_size_x = 1
+    TB_size_y = 1
+    with open(cu_file, "r") as f:
+        for line in f.readlines():
+            if not "[thread_extent]" in line:
+                continue
+            val = int(line.split(" ")[-1])
+            if "blockIdx.x" in line:
+                TB_count_x = val
+            if "blockIdx.y" in line:
+                TB_count_x = val
+            if "threadIdx.x" in line:
+                TB_size_x = val
+            if "threadIdx.y" in line:
+                TB_size_x = val
+    return TB_size_x * TB_size_y, TB_count_x * TB_count_y
+
+def estimate_TB(ptx_parser, hw_config, batch_size)
+    # Estimate the given the performance of one TB 
+    # need to know how many TBs can run concurrently (batch_size) to determine the DRAM traffic
     TB_perf["pipeline_latency"] = 
     TB_perf["transfer_latency"] = 
     TB_perf["compute_latency"] = 
     return TB_perf
 
 def estimate_batch(ptx_parser, hw_config, batch_size, TB_perf):
-    # Estimate the given the performance of one TB
+    # Estimate the given the performance of one TB batch, given the performance of single TBs
     pass
 
-def estimate_all(source_parser, ptx_parser, hw_config):
+def estimate_all(cu_file, ptx_parser, hw_config):
     # The process of estimating performance
     # input:
     #   ptx_parser: object containing all information related to operators
@@ -28,26 +54,31 @@ def estimate_all(source_parser, ptx_parser, hw_config):
     # 2, Estimate the performance of one TB batch (all TBs that can execute in parallel)
     # 3, Estimate the total elapsed time
     
+    TB_size, TB_count = get_TB_size_count(cu_file)
+    # how many TBs can execute in parallel (size of one TB batch)
+    batch_size = min(TB_count,
+                     hw_config.TF_SIZE * 1024 // ptx_parser.register_size_used())
+    if ptx_parser.shared_memory_used() != 0:
+        batch_size = min(batch_size,
+                         hw_config.SHARED_MEMORY_SIZE * 1024 // ptx_parser.shared_memory_used())
+    batch_count = TB_count // batch_size # number of TB batches
+    rem_size = TB_count - batch_count * batch_size
+
     # step 1: estimating the performance of a single TB
-    TB_perf = estimate_TB(ptx_parser, hw_config)
+    TB_perf_batch = estimate_TB(ptx_parser, hw_config, batch_size)
+    TB_perf_rem = estimate_TB(ptx_parser, hw_config, rem_size)
     
     # step 2: estimating the performance of a TB batch
-    TB_size = # how many threads in one TB
-    batch_size = # how many TBs can execute in parallel
-
-    TB_count = ... # number of TBs
-    batch_count = TB_count / batch_size # number of TB batches
-    batch_latency = estimate_batch(ptx_parser, hw_config, batch_size, TB_perf)
-
-    rem_size = TB_count - batch_count * batch_size
-    rem_latency = estimate_batch(ptx_parser, hw_config, rem_size, TB_perf)
+    batch_latency = estimate_batch(ptx_parser, hw_config, batch_size, TB_perf_batch)
+    rem_latency = estimate_batch(ptx_parser, hw_config, rem_size, TB_perf_rem)
     
     # step 3: estimating the final performance
     return batch_latency * batch_count + rem_latency
 
 def main(args):
-    ptx_p = ptx_parser(args.ptx_file)
-    pass
+    ptx_ = ptx_parser(args.ptx_file)
+    cu_ = args.cu_file
+    estimate_all(cu_, ptx_, GPU_Config())
 
 
 if __name__ == "__main__":
